fdd371115557c8ae4f8592a261b3985642c32bda
Nusrath <nusrathjahan0204@gmail.com>
2025-08-11 23:04:53 +0200

    added  DB_PASSWORD for render

--- api/src/database_client.js
+++ api/src/database_client.js
@@ -6,6 +6,7 @@ const connection = knex({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_NAME,
     ssl:
       process.env.DB_USE_SSL === "true" ? { rejectUnauthorized: false } : false,
